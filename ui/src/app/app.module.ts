ecda6b48aff36573fcc1363f202fee0f6d4eb7d8
Rolf1e <tigran.slama@laposte.net>
2020-03-18 00:17:19 +0100

    Reorganize package

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BannerComponent } from './banner/banner.component';
-import { MenuComponent } from './menu/menu.component';
-import { SigninComponent } from './login/signin/signin.component';
-import { SlideShowComponent } from './slide-show/slide-show.component';
+import { BannerComponent } from './index/banner/banner.component';
+import { MenuComponent } from './index/menu/menu.component';
+import { SigninComponent } from './index/login/signin/signin.component';
+import { SlideShowComponent } from './index/slide-show/slide-show.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NewsComponent } from './news/news.component';
-import { YoutubeIncrustationComponent } from './youtube-incrustation/youtube-incrustation.component';
+import { NewsComponent } from './index/news/news.component';
+import { YoutubeIncrustationComponent } from './index/youtube-incrustation/youtube-incrustation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { IndexComponent } from './index/index.component'
