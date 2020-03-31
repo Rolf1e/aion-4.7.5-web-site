4b563f757c5f0db03f2bca6059df79f93dc11f37
Arhtur <arthur.remond@protonmail.com>
2020-03-31 23:25:09 +0200

    add home page

---
+++ client/pages/forum.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div>
+     <h1> Je suis sur la page forum</h1>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Form"
+    }
+</script>
+
+<style scoped>
+
+</style>
