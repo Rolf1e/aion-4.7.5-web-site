2f2a5ce03213f8e2da311a21bc3497ab24a63e86
Rolfie <tigran.slama@laposte.net>
2020-04-17 01:15:14 +0200

    Template to handle html content

--- server/src/main/java/com/aion/server/controller/RegisterController.java
+++ server/src/main/java/com/aion/server/controller/RegisterController.java
@@ -6,6 +6,7 @@ import com.aion.server.service.infra.dto.OutputUserInfos;
 import com.aion.server.service.infra.exception.UserExistException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- server/src/main/java/com/aion/server/controller/ShopController.java
+++ server/src/main/java/com/aion/server/controller/ShopController.java
@@ -7,6 +7,7 @@ import com.aion.server.service.infra.dto.InputUserInfos;
 import com.aion.server.service.infra.exception.UserDoesntExistException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
