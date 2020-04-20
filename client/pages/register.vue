ba0b897e6dd0ee2e00061c08a55a530d2e2c01ee
Arhtur <arthur.remond@protonmail.com>
2020-04-20 21:19:13 +0200

    add : register

--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,96 +1,125 @@
 <template>
-  <div class="container">
+    <div class="container">
 
-    <h1 class="title"> Créer un compte </h1>
+        <h1 class="title"> Créer un compte </h1>
 
-    <b-field label="Pseudo">
-      <b-input v-model="username" @keyup.native="error.show = false" @keyup.native.enter="login"></b-input>
-    </b-field>
+        <b-field label="Pseudo">
+            <b-input v-model="username" @keyup.native="error.show = false" @keyup.native.enter="login"></b-input>
+        </b-field>
 
-    <b-field label="Email">
-      <b-input v-model="email" @keyup.native="error.show = false" @keyup.native.enter="login"></b-input>
-    </b-field>
+        <b-field label="Email">
+            <b-input v-model="email" @keyup.native="error.show = false" @keyup.native.enter="login"></b-input>
+        </b-field>
 
-    <b-field label="Mot de passe">
-      <b-input type="password" v-model="password" @keyup.native="error.show = false" @keyup.native.enter="login"
-               password-reveal></b-input>
-    </b-field>
+        <b-field label="Mot de passe">
+            <b-input type="password" v-model="password" @keyup.native="error.show = false" @keyup.native.enter="login"
+                     password-reveal></b-input>
+        </b-field>
 
-    <b-field label="Comfirmer mot de passe">
-      <b-input type="password" v-model="confirmPassword" @keyup.native="error.show = false" @keyup.native.enter="login"
-               password-reveal></b-input>
-    </b-field>
+        <b-field label="Comfirmer mot de passe">
+            <b-input type="password" v-model="confirmPassword" @keyup.native="error.show = false"
+                     @keyup.native.enter="login"
+                     password-reveal></b-input>
+        </b-field>
 
-    <b-message v-if="error.show" type="is-danger">
-      {{ this.error.message }}
-    </b-message>
+        <b-message v-if="error.show" type="is-danger">
+            {{ this.error.message }}
+        </b-message>
 
 
+        <b-button type="button is-primary" @click="login">Connexion</b-button>
 
-
-    <b-button  type="button is-primary" @click="login">Connexion</b-button>
-
-  </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "register",
 
-    data() {
-      return {
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        error: {
-          show: false,
-          message: ''
+    import Swal from 'sweetalert2'
+
+    export default {
+        name: "register",
+
+        data() {
+            return {
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+                error: {
+                    show: false,
+                    message: ''
+                },
+            }
         },
-      }
-    },
 
-    methods: {
-      async login() {
+        methods: {
+            async login() {
 
-        if (this.password === this.confirmPassword && this.password != '') {
+                this.openModal()
 
-          const response = await this.$axios.post('http://localhost:8080/register', {
-            "username": this.username,
-            "password": this.password,
-            "mail": this.email
-          })
+                if (this.password === this.confirmPassword && this.password != '') {
 
-          this.openToast('Un mail a été envoyé !!')
+                    const {data: response} = await this.$axios.post('http://localhost:8080/register', {
+                        "username": this.username,
+                        "password": this.password,
+                        "mail": this.email
+                    })
 
-        } else {
-          this.error.message = 'Les mots de passe ne sont pas identiques'
-          this.error.show = true
+                    console.log(response)
+
+                    if (response.error === false) {
+                        Swal.fire({
+                            title: 'Votre compte a été créé',
+                            text: 'Veuillez vérifier votre boite mail pour valider votre compte',
+                            icon: 'success',
+                            confirmButtonText: 'Cool'
+                        }).then((result) => {
+                            if (result.value) {
+                                this.$router.push({path: '/'})
+                            }
+                        })
+
+                    } else {
+                        this.error.message = 'Mot de passe ou email non valide'
+                        this.error.show = true
+                        this.username = ''
+                        this.email = ''
+                        this.password = ''
+                        this.confirmPassword = ''
+                    }
+
+                } else {
+                    this.error.message = 'Les mots de passe ne sont pas identiques'
+                    this.error.show = true
+                }
+
+                /*
+                if (this.email == 'arthur' && this.password == 'mdp') {
+                  console.log('le mdp est bon')
+                  let token = 'mon_token'
+                  this.$store.dispatch('auth/loadToken', token )
+                  this.$router.push({path : '/'})
+                } else {
+                  this.error.show = true
+                }
+                */
+
+            },
+
+            openToast(message) {
+                this.$buefy.toast.open({
+                    message: message,
+                    type: 'is-success',
+                    duration: 15000
+                })
+            },
+
+            openModal() {
+
+            }
+
         }
-
-        /*
-        if (this.email == 'arthur' && this.password == 'mdp') {
-          console.log('le mdp est bon')
-          let token = 'mon_token'
-          this.$store.dispatch('auth/loadToken', token )
-          this.$router.push({path : '/'})
-        } else {
-          this.error.show = true
-        }
-        */
-
-      },
-
-      openToast(message) {
-        this.$buefy.toast.open({
-          message: message,
-          type: 'is-success',
-          duration : 15000
-        })
-      }
-
     }
-  }
 </script>
 
 <style scoped>
