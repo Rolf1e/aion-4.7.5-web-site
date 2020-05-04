3a48bd617c419cc8d5e41dd5089fd17363f75968
Rolfie <tigran.slama@laposte.net>
2020-05-04 02:29:16 +0200

    Nore logs

--- server/src/main/java/com/aion/server/controller/PlayerInformationController.java
+++ server/src/main/java/com/aion/server/controller/PlayerInformationController.java
@@ -3,6 +3,7 @@ package com.aion.server.controller;
 import com.aion.server.database.entity.game.Player;
 import com.aion.server.service.PlayerInformationService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class PlayerInformationController {
@@ -19,12 +21,14 @@ public class PlayerInformationController {
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/list-players")
     public List<Player> getListPlayers(@RequestParam(value = "idUser") final int idUser) {
+        log.info("Get player informations for user {}", idUser);
         return playerInformationService.getWholeInfosForPlayer(idUser);
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/check-players-exist")
     public boolean checkPlayersExist(@RequestParam(value = "name") final String name) {
+        log.info("Check if player {} exist", name);
         return playerInformationService.checkPlayerExist(name);
     }
 }
