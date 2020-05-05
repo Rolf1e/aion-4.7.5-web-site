5da8dcbeecd670e7eab9957a292aa2523a48f067
Rolfie <tigran.slama@laposte.net>
2020-05-05 14:54:04 +0200

    cors try resolve

--- server/src/main/java/com/aion/server/controller/ShopController.java
+++ server/src/main/java/com/aion/server/controller/ShopController.java
@@ -106,7 +106,7 @@ public class ShopController {
         }
     }
 
-    @CrossOrigin(origins = "http://51.178.130.119:3000")
+    @CrossOrigin
     @GetMapping(value = "/list-shop")
     public List<Shop> getListShopItem(@RequestParam(value = "category", defaultValue = "all") final String category) {
         if (category.equals("all")) {
@@ -117,7 +117,7 @@ public class ShopController {
         return shopService.getShopListByCategory(category);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     @GetMapping(value = "/list-shop/category")
     public List<String> getListCategory() {
         log.info("Get shop list by categories");
